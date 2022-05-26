a178482ab3746a9baf4851e3d04af53ef1d3184b
celadia <celadia@student.school-21.ru>
2022-05-26 14:38:32 +0300

    Добавлены сторонние библиотеки

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,6 +1,19 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <stdio.h>
+# include <stdio.h> /* printf, perror */
+# include <stdlib.h> /* malloc, free, exit, getenv */
+# include <string.h> /* strerror */
+# include <fcntl.h> /* open */
+#include <term.h> /* tcsetattr, tcgetattr, tgetent, tgetflag, tgetnum,
+ * tgetstr, tgoto, tputs */
+# include <unistd.h> /* write, read, access, close, fork, getcwd, chdir,
+ * unlink, execve, dup, dup2, pipe, isatty, ttyname, ttyslot */
+# include <sys/types.h> /* wait, waitpid, opendir, readdir, closedir */
+# include <sys/stat.h> /* stat, lstat, fstat */
+# include <sys/ioctl.h> /* ioctl */
+# include <signal.h> /* signal, sigaction, sigemptyset, sigaddset, kill */
+# include <readline/readline.h> /* readline, rl_clear_history,
+ * rl_on_new_line, rl_replace_line, rl_redisplay, add_history */
 
 #endif //MINISHELL_H
--- source/main.c
+++ source/main.c
@@ -1,9 +1,4 @@
-//#include "minishell.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+#include "minishell.h"
 
 int	main(void)
 {
