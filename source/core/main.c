834746887f900ef415d55e49cfd5c0ff10cd60e8
celadia <celadia@student.school-21.ru>
2022-06-01 14:20:46 +0300

    2

--- source/core/main.c
+++ source/core/main.c
@@ -1,44 +1,13 @@
 #include "minishell.h"
 
-#define PATH_SIZE 256
-
-char	*get_dir_name(void)
-{
-	char	dir_path[PATH_SIZE];
-	char	*dir_name;
-	int		size;
-	int		len;
-	int		i;
-
-	size = 0;
-	i = -1;
-	getcwd(dir_path, PATH_SIZE);
-	len = ft_strlen(dir_path);
-	while (dir_path[len -1] != '/' && --len)
-		size++;
-	dir_name = (char *)malloc(sizeof(char) * (size + 3));
-	while (dir_path[len + ++i])
-		dir_name[i] = dir_path[len + i];
-	dir_name[i++] = ' ';
-	dir_name[i++] = '>';
-	dir_name[i++] = ' ';
-	dir_name[i] = '\0';
-	return (dir_name);
-}
-
 int	main(int argc, char **argv, char **envp)
 {
-	char	*dir_name;
-	char	*rd_line;
+	char	*rl_str;
+	char	*env;
 
-	dir_name = get_dir_name();
-	while (1)
-	{
-		rd_line = readline(dir_name);
-		add_history(rd_line);
-		rl_redisplay();
-		free(rd_line);
-	}
-	free(dir_name);
+	rl_str = readline("minishell > ");
+	env = getenv("PATH");
+	printf("%s\n", env);
+	free(rl_str);
 	return (0);
 }
